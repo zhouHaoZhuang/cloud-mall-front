c25c91af6ce554fd2716b446ea939642a9f99b44
韩启航 <1878806686@qq.com>
2022-01-23 14:29:37 +0800

    添加全局过滤器

--- plugins/mixins.js
+++ plugins/mixins.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import * as Directives from '../assets/js/directives/directive1.js'
+import * as Filters from '../assets/js/filters/index'
 
 // 全局指令
 Vue.use(Directives)
+// 全局过滤器
+Vue.use(Filters)
 
 // 混入methods
 Vue.mixin({
