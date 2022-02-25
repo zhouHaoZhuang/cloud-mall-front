e0ff70c65270edef60bd11d92d7a684975eeb1bb
韩启航 <1878806686@qq.com>
2022-02-25 21:16:39 +0800

    地址

--- config/env.js
+++ config/env.js
@@ -1,8 +1,8 @@
 // 请求地址头
 const baseUrl = {
   local: 'http://site.ydidc.com/server',
-  dev: 'http://t946471742443573248.site.ydidc.com',
-  test: 'http://t647893.site.zjyundun.com',
+  dev: 'http://t946471742443573248.site.ydidc.com/serve',
+  test: 'http://t647893.site.zjyundun.com/serve',
   prod: '/server'
 }
 // 请求头携带domain参数
