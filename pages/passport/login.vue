5ad65fed4773a72f84da5bca9f5c448f16846a97
zhouHaoZhuang <88356815+zhouHaoZhuang@users.noreply.github.com>
2022-03-03 18:13:11 +0800

    修改验证功能

--- pages/passport/login.vue
+++ pages/passport/login.vue
@@ -111,17 +111,8 @@ export default {
             trigger: 'blur'
           },
           {
-            validator: (rule, value, callback) => {
-              if (value === '') {
-                callback()
-              } else {
-                if (this.setpswdblurfns()) {
-                  callback()
-                } else {
-                  callback(new Error('请输入正确的密码格式'))
-                }
-              }
-            },
+            pattern: this.pwdReg,
+            message: '密码格式不正确',
             trigger: 'blur'
           }
         ]
